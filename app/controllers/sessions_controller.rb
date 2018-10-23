e3420b86a5c0843a7a46dff3212a35b98d6b574c
alcasinoroyale <alcasinoroyale@gmail.com>
2018-10-22 22:00:00 -0400

    added methods for sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
-  
+
   def create
+    @user = User.find_by(username: params[:username])
+       if @user && @user.authenticate(params[:password])
+         session[:user_id] = @user.id
+         redirect_to user_path(@user)
+       else
+         redirect_to 'welcome/home'
+      end
   end
 
   def destroy
+    session.destroy
+    redirect_to root_url
   end
 end
