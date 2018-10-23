6d18ccb97a6de4215177b6f6c5f9fd5687922fcc
alcasinoroyale <alcasinoroyale@gmail.com>
2018-10-22 22:53:49 -0400

    fixed sessions controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
 class SessionsController < ApplicationController
   def new
-    @user = User.new
+    if logged_in?
+      redirect_to user_path(@current_user)
+    else
+      @user = User.new
+    end
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-       if @user && @user.authenticate(params[:password])
+    @user = User.find_by(username: params[:user][:username])
+       if @user && @user.authenticate(params[:user][:password])
          session[:user_id] = @user.id
          redirect_to user_path(@user)
        else
