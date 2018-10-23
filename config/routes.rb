57fe26e1a197d3d4270f2e917109e60ef6eedff2
alcasinoroyale <alcasinoroyale@gmail.com>
2018-10-22 22:01:32 -0400

    added routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :games
   resources :sessions, only: [:new, :create, :destroy]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  post '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
