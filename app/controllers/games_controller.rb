730eee8694d37086104538a70ca54d714b667b46
alcasinoroyale <alcasinoroyale@gmail.com>
2018-10-24 15:25:44 -0400

    starting to build out methods for games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,25 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
   end
 
   def new
+    @game = Game.new
   end
 
   def create
+    @game = Game.create(game_params)
+    redirect_to game_path(@game)
   end
 
   def edit
+    @game = Game.find_by(id: params[:id])
   end
 
   def update
+    @game = Game.find_by(id: params[:id])
+    @game.update(game_params)
+    redirect_to game_path(@game)
   end
 
   def show
