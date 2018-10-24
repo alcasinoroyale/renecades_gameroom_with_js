0c6972203617d403e1caa4b7e7accfb99086e344
alcasinoroyale <alcasinoroyale@gmail.com>
2018-10-24 15:22:01 -0400

    added test accounts and fixed nav bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+geralt = User.create(username: "Geralt of Rivia", email: "geralt@gmail.com", bio: "Loves Role-playing Games", favorite_game: "The Witcher", password: "geralt")
+yoshi = User.create(username: "Yoshi", email: "yoshi@gmail.com", bio: "Nintendo is my childhood", favorite_game: "Mario Kart", password: "nintendo")
