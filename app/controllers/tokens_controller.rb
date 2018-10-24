f3fcad6edccefcc8a7daad8641af044d87131635
alcasinoroyale <alcasinoroyale@gmail.com>
2018-10-24 17:39:31 -0400

    created tokens controller and actions

---
+++ app/controllers/tokens_controller.rb
@@ -0,0 +1,16 @@
+class TokensController < ApplicationController
+    def new
+      @token = Token.new
+    end
+
+    def create
+      @token = Token.create(token_params)
+      redirect_to user_path(@token.user)
+    end
+
+    private
+
+    def token_params
+      params.require(:token).permit(:user_id, :game_id)
+  end
+end
