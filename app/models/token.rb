835f953d233dbc7c56fd2e78ff809b45a9b72e7c
alcasinoroyale <alcasinoroyale@gmail.com>
2018-10-25 16:36:02 -0400

    adds reward points to player profiles

--- app/models/token.rb
+++ app/models/token.rb
@@ -3,7 +3,8 @@ class Token < ApplicationRecord
   belongs_to :game
 
   def play_game
-    user.reward_points += game.reward_points
+    "user.reward_points" + "game.reward_points"
+    user.save
     "Thanks for playing #{game.name}!"
   end
 end
