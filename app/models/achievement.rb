8e008c2f51ce249bf754be154a7c1885b4efcb26
alcasinoroyale <alcasinoroyale@gmail.com>
2018-11-01 18:49:50 -0400

    adding notes for earn achievement method

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -1,4 +1,11 @@
 class Achievement < ApplicationRecord
   belongs_to :game
   belongs_to :user
+
+  def earn_achievement
+    #if user has #{integer} reward points
+    #puts "You have earned the title of #{achievement.name}"
+    #add achievement.name to User's Profile as their rank.
+    #if you reach next achievement level, change rank
+  end
 end
