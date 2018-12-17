8e0630057f902c1bf26d6d6ae981fa15442b5243
alcasinoroyale <alcasinoroyale@gmail.com>
2018-12-17 15:37:51 -0500

    Start User Reward Points at 0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+      @user.reward_points = "0"
     if @user.save
       session[:user_id] = @user.id
       flash[:message] = "User created successfully!"
