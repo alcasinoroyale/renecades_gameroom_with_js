69ddbdffb3e73e985ad382244c9034de793a1363
alcasinoroyale <alcasinoroyale@gmail.com>
2019-02-11 15:54:49 -0500

    Add has_many relationships to user and game serializers

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :email, :bio, :favorite_game, :ranks, :reward_points
+  has_many :tokens
+  has_many :games, through: :tokens
+  has_many :created_games, class_name: "Game", foreign_key: "creator_id"
+  has_many :categories, through: :created_games
 end
