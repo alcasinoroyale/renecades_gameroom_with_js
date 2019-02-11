a1b563171517742adb15d88fdcf1de9740b3dbe7
alcasinoroyale <alcasinoroyale@gmail.com>
2019-02-11 14:34:06 -0500

    Create User Serializer and add attributes for User and Game Serializers

--- app/serializers/game_serializer.rb
+++ app/serializers/game_serializer.rb
@@ -1,3 +1,3 @@
 class GameSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :objective, :number_of_players, :reward_points, :genre, :category_id
+  attributes :id, :name, :description, :objective, :number_of_players, :reward_points, :genre, :category_id, :creator_id
 end
