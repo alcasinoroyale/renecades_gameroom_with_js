ded1df6b881c61f9a7b61931b2e29de6e284766a
alcasinoroyale <alcasinoroyale@gmail.com>
2019-02-15 17:38:17 -0500

    Setting Up Game constructor with JS

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,14 +1,17 @@
-function Game(id, name, description, objective, number_of_players, reward_points, genre){
-  this.id = id;
-  this.name = name;
-  this.description = description;
-  this.objective = objective;
-  this.number_of_players = number_of_players;
-  this.reward_points = reward_points;
-  this.genre = genre;
+class Game {
+  constructor(id, name, description, objective, number_of_players, reward_points, creator_id, category_id, genre) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.objective = objective;
+    this.number_of_players = number_of_players;
+    this.reward_points = reward_points;
+    this.creator_id = creator_id;
+    this.category_id = category_id;
+    this.genre = genre;
 };
 
-$(function() {
+function nextGame(){
   $(".js-nextgame").on("click", function() {
     const nextId = $(this).data("id") + 1;
     $.get("/games" + nextId + ".json", function(data){
