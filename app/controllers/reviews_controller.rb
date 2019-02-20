93eb50a95a091bec11c2c22b901d6d8c8faa8d44
alcasinoroyale <alcasinoroyale@gmail.com>
2019-02-20 15:25:54 -0500

    Building out the reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,28 @@
 class ReviewsController < ApplicationController
+
   def index
     @reviews = @game.reviews
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.game_id = @game.id
+
+    if @review.save
+      redirect_to game_path(@game)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :game_id, :user_id)
   end
 end
