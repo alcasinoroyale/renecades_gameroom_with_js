77e4dd19bde94b973ec09d13b4914b1a19cebd8d
alcasinoroyale <alcasinoroyale@gmail.com>
2019-02-20 15:04:00 -0500

    Begin to build forms and links for reviews

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,7 @@
 class Game < ApplicationRecord
   has_many :tokens
   has_many :users, through: :tokens
-  has_many :reviews
+  has_many :reviews, through: :users
   belongs_to :category
   belongs_to :creator, class_name: "User", foreign_key: "creator_id"
   validates :name, :description, :objective, :number_of_players, :reward_points, :genre, :category_id, presence: true
